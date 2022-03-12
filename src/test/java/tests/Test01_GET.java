af89f55ad41758c06047fad7347ea5b6536a4b9f
shivam dubey <dubeyshivam890@gmail.com>
2022-03-12 14:12:22 +0530

    my commit

---
+++ src/test/java/tests/Test01_GET.java
@@ -0,0 +1,46 @@
+package tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+
+public class Test01_GET 
+
+{	@Test
+	void test01()
+	{
+		
+		Response resp = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(resp.getBody().asString());
+		System.out.println(resp.asString());
+		System.out.println(resp.getStatusCode());
+		System.out.println(resp.getStatusLine());
+		System.out.println(resp.getHeader("content type"));
+		System.out.println(resp.getTime());
+		
+		int statuscode = resp.getStatusCode();
+		Assert.assertEquals(statuscode, 200);
+	}
+
+	
+	@Test
+	void test02() 
+	
+	{
+		RestAssured.given().get("https://reqres.in/api/users?page=2").then().statusCode(200).log().all();
+		
+		
+	}
+	
+	@Test
+	void test03()
+	{
+		
+		RestAssured.given().get("https://httpbin.org/").then().statusCode(200).log().all();
+		
+	}
+	
+	
+}
