9e5149974e96bc0b5a5200cfdec62df4be71ee78
shivam dubey <95178183+shivusrd@users.noreply.github.com>
2022-03-19 13:47:40 +0530

    hi

--- src/test/java/tests/Test02_POST.java
+++ src/test/java/tests/Test02_POST.java
@@ -8,6 +8,9 @@ import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.gherkin.model.Given;
 import com.google.gson.JsonObject;
+
+import io.restassured.http.ContentType;
+
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -33,9 +36,69 @@ public class Test02_POST
 		System.out.println(req);
 		System.out.println(req.toJSONString());
 
-		given().body(req.toJSONString()).
-		when().post("https://reqres.in/api/users").then().statusCode(201);
+		given().
+		header("Content-Type","aplication/json").
+		contentType(ContentType.JSON).
+		accept(ContentType.JSON).
+		body(req.toJSONString()).
+		when().post("https://reqres.in/api/users").then().statusCode(201).log().all();
 
 	}
+	
+	
+
+		@Test
+		public void test02_put() 
+		{
+
+
+			JSONObject req = new JSONObject();
+
+			req.put("name", "shivam");
+			req.put("job", "QA");
+			System.out.println(req);
+			System.out.println(req.toJSONString());
+
+			given().
+			header("Content-Type","aplication/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(req.toJSONString()).
+			when().put("https://reqres.in/api/users/2").then().statusCode(200).log().all();
+
+		}
+		
+		@Test
+		public void test03_patch() 
+		{
+
+
+			JSONObject req = new JSONObject();
+
+			req.put("name", "shivam");
+			req.put("job", "QA");
+			System.out.println(req);
+			System.out.println(req.toJSONString());
+
+			given().
+			header("Content-Type","aplication/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(req.toJSONString()).
+			when().patch("https://reqres.in/api/users/2").then().statusCode(200).log().all();
+
+		}
+		
+		@Test
+		public void test04_patch() 
+		{
+
+
+			
+			when().delete("https://reqres.in/api/users/2").then().statusCode(204).log().all();
+
+		}
+		
+		
 
 }
